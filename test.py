92efbdeeb126f5fc52a0f195ec0eee48ba7447ef
Hedi <hedixia@umail.ucsb.edu>
2018-06-01 00:28:21 -0700

    x

--- test.py
+++ test.py
@@ -10,14 +10,16 @@ from kernel import kernel
 import random 
 import matplotlib.pyplot as plt
 
+os.chdir(os.path.dirname(__file__))
+
 x = 10
 dataset = Dataset([[i/20, i//10] for i in range(10*x)])
 ker = kernel("ra", [1])
 
 temp_sac1 = SA_clust(dataset, ker, 10, range(100), 10, False)
 temp_sac1.build()
-temp_sac1.show()
+temp_sac1.show("SA")
 
 temp_sac2 = SA2_clust(dataset, ker, 10, 2, range(100), 10, False)
 temp_sac2.build()
-temp_sac2.show()
+temp_sac2.show("SA2")
