2cbd5c32fe8994461e49bfcdc3c8eb5a8f396667
Hedi <hedixia@umail.ucsb.edu>
2018-06-01 13:46:11 -0700

    x

--- Dataset.py
+++ Dataset.py
@@ -1,15 +1,19 @@
 import numpy as np 
 
 class Dataset:
-	def __init__ (self, matrix):
+	def __init__ (self, matrix=[]):
 		self.M = np.matrix(matrix)
 		self.n = len(matrix)
-		
+
 	def __len__ (self):
-		return self.n 
-	
-	def __getitem__ (self, i):
-		return self.M[i,:]
-		
+		return self.n
+
+	def __getitem__ (self, item):
+		return self.M[item, :]
+
+	def __add__ (self, other):
+		return Dataset(np.concatenate((self.M, other.M), axis=0))
+
 	def getT (self):
 		return Dataset(self.M.getT())
+
