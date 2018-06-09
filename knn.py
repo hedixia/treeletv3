ce0642c3786c7dee6757739821bd5186ad50ba67
Hedi <hedixia@umail.ucsb.edu>
2018-06-08 23:53:22 -0700

    x

---
+++ knn.py
@@ -0,0 +1,26 @@
+from classifier import classifier
+class KNN (classifier):
+	def __init__(self, dataset_ref=[], trlabel=[], slice=False, measure=lambda x,y : 1, K=0):
+		super().__init__(dataset_ref, slice)
+		self.K = K
+
+	def build(self):
+		super().build()
+		if self.K is not 0:
+			Tval = 0
+			Fval = 0
+			for i in self.slice:
+				if self.predict(self.dataset_ref[i]) == self.labels[i]:
+					Tval += 1
+				else:
+					Fval += 1
+			self.trerr = Tval / (Tval + Fval)
+		else:
+			pass #Cross Validation to select K
+
+
+	def predict (self, test_data):
+		MTD = {x:self.measure(self.dataset_ref[x], test_data) for x in self.slice}
+		first_K = sorted(MTD.keys(), MTD.get)[:self.K]
+		K_lab = [self.labels[i] for i in first_K]
+		return max(set(K_lab), key=K_lab.count)
