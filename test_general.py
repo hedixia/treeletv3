48757fbd1d8fb24e62d117d429ae668a0c16eb39
Hedi <hedixia@umail.ucsb.edu>
2018-06-20 16:22:49 -0700

    x

--- test_general.py
+++ test_general.py
@@ -1,25 +1,21 @@
-import csv
 import os
 import random
 
 import numpy as np
 
-from Dataset import Dataset
-from kernel import kernel
-from treelet_classifier import treelet_classifier, MajorityVote
-from SA2_method import SA2_clust
-from cluster_classification_mix import cluster_classification_mix
 from dataset_loader import loadDataset
-
+from kernel import kernel
+from treelet_classifier import treelet_classifier
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
-datadir = r"C:\Users\Hedi Xia\Desktop\datasets\MPE\\"
-training_percentage = 0.5
-recnum = 1
+datadir = r"C:\Users\Hedi Xia\Desktop\datasets\ADD\\"
+training_percentage = 0.6
+recnum = 4
 
-#load and split data
+# load and split data
 alldat = loadDataset(datadir, ["dat.csv"], datatype=float, labcol=0)
 n = len(alldat)
+print(n)
 trn = int(n * training_percentage)
 tsn = n - trn
 trslice = random.sample(range(n), trn)
@@ -27,7 +23,7 @@ tsslice = [i for i in range(n) if i not in trslice]
 trdat = alldat.getSlice(trslice)
 tsdat = alldat.getSlice(tsslice)
 
-#computation
+# computation
 variance = [trdat.Var, tsdat.Var]
 print("var: ", variance)
 ker = kernel("ra", [np.sqrt(variance[0])])
@@ -38,16 +34,18 @@ print("training error: ", trc.training_error())
 testerror = sum([prediction_lab[i] != tsdat.label[i] for i in range(tsn)]) / tsn
 print("test error: ", testerror)
 
-#write test to file
+# write test to file
 writeL = [
-	"Test No." + str(recnum).zfill(3),
-	"Training Error: " + str(trc.training_error()),
-	"Test Error: " + str(testerror),
-	"Training data size: " + str(trn),
-	"Test data size: " + str(tsn),
-	"Training data slice: " + str(trslice)
+	"Test No." + str(recnum).zfill(3) + "\n",
+	"Data dir: " + datadir[-5:-2] + "\n",
+	"Training Error: " + str(trc.training_error()) + "\n",
+	"Test Error: " + str(testerror) + "\n",
+	"Training data size: " + str(trn) + "\n",
+	"Test data size: " + str(tsn) + "\n",
+	"Training data percent: " + str(training_percentage) + "\n",
+	"Training data slice: " + str(sorted(trslice)) + "\n"
 ]
 
-openfile = open ("record" + str(recnum).zfill(3) + ".txt", "w")
+openfile = open("record" + str(recnum).zfill(3) + ".txt", "w")
 openfile.writelines(writeL)
 openfile.close()
