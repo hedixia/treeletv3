1054828c1e032c35ee73eba85150c425febdc2d4
Hedi <hedixia@umail.ucsb.edu>
2018-07-20 16:31:14 -0400

    x

--- treelet_dimred.py
+++ treelet_dimred.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from treelet import treelet
+from scipy.sparse import coo_matrix
 
 
 def listgen (L, start):
@@ -27,42 +28,37 @@ class treelet_dimred:
 
 	# Treelet Transform
 	def transform (self, v, k=False, epsilon=0):
-		v = np.array(v) - self.avedat
+		v = np.matrix(v) - self.avedat
 		k = k if k else self.n - 1
 		for iter in range(k):
 			(scv, cgs, cos_val, sin_val) = self.transform_list[iter]
-			temp_scv = cos_val * v[scv] - sin_val * v[cgs]
-			temp_cgs = sin_val * v[scv] + cos_val * v[cgs]
-			v[scv] = temp_scv
-			v[cgs] = temp_cgs
+			temp_scv = cos_val * v[:, scv] - sin_val * v[:, cgs]
+			temp_cgs = sin_val * v[:, scv] + cos_val * v[:, cgs]
+			v[:, scv] = temp_scv
+			v[:, cgs] = temp_cgs
 		if epsilon == 0:
 			return (v, None)
 		else:
-			newdict = {}
-			newvec = np.zeros(n - k)
-			for i in range(n):
-				if i < k:
-					if abs(v[self.dfrk[i]]) > epsilon:
-						newdict[self.dfrk[i]] = v[self.dfrk[i]]
-				else:
-					newvec[i] = v[self.dfrk[i + k]]
-			return (newvec, newdict)
+			scaling_part = v[:, self.dfrk[:k]]
+			difference_part = v[:, self.dfrk[k:]]
+			difference_mat = coo_matrix(abs(difference_part) > epsilon).multiply(difference_part)
+			return (scaling_part, difference_mat)
 
-	def inverse_transform (self, v, diffdict={}):
-		k = self.n - len(v)
-		if k != 0:
-			newv = np.zeros(self.n)
-			for i in range(len(v)):
-				newv[self.dfrk[i + k]] = v[i]
-			v = newv
+	def inverse_transform (self, scaling_part, difference_mat=False):
+		scaling_part = np.matrix(scaling_part)
+		rows = scaling_part.shape[0]
+		cols = self.n
+		k = cols - scaling_part.shape[1]
+		v = np.matrix(np.zeros((rows, cols)))
+		v[:, self.dfrk[k:]] = scaling_part
 		for iter in reversed(self.transform_list):
 			(scv, cgs, cos_val, sin_val) = iter
-			temp_scv = cos_val * v[scv] + sin_val * v[cgs]
-			temp_cgs = -sin_val * v[scv] + cos_val * v[cgs]
-			v[scv] = temp_scv
-			v[cgs] = temp_cgs
+			temp_scv = cos_val * v[:, scv] + sin_val * v[:, cgs]
+			temp_cgs = -sin_val * v[:, scv] + cos_val * v[:, cgs]
+			v[:, scv] = temp_scv
+			v[:, cgs] = temp_cgs
 		for i in diffdict:
-			v[i] += diffdict[i]
+			v[:, self.dfrk[:k]] += difference_mat
 		return v + self.avedat
 
 	def cluster (self, k):
