a2e173faffd4283eed17dd8c385bb9ff28c4aee2
Hedi <hedixia@umail.ucsb.edu>
2018-07-23 21:11:18 -0400

    x

--- treelet_clust.py
+++ treelet_clust.py
@@ -8,9 +8,14 @@ class treelet_clust(clust):
 	def __init__ (self, dataset_ref, kernel, slice=False, num_clust=1, all_kernel=False):
 		super().__init__(dataset_ref, slice)
 		self.kernel = kernel
-		if num_clust < 1:
-			raise ValueError
-		self.num_clust = num_clust
+
+		if num_clust is 0: #Auto-find clust num
+			self.num_clust = 1
+			self.clustnum_estimate = True
+		else:
+			self.num_clust = num_clust
+			self.clustnum_estimate = False
+
 		if all_kernel:
 			temp_slice = np.array(self.slice, dtype=np.intp)
 			self.A = all_kernel[temp_slice[:, np.newaxis], temp_slice]
@@ -24,6 +29,8 @@ class treelet_clust(clust):
 		trl = treelet(self.A, self.psi)
 		trl.fullrotate()
 		self.cltree = trl.tree()
+		if self.clustnum_estimate:
+			self.find_clust_num(trl.dendrogram_list)
 		temp_labels = list(range(self.size))
 		for i in range(self.size - self.num_clust):
 			temp_labels[self.cltree[i][1]] = self.cltree[i][0]
@@ -45,6 +52,15 @@ class treelet_clust(clust):
 		closest = max(self.slice, key=linkf)
 		return self.labels[closest]
 
+	def find_clust_num (self, dendrogram_list):
+		#find the first gap with 1
+		for i in range(len(self)-1):
+			if i is 0:
+				continue
+			else:
+				if np.abs(dendrogram_list[i-1] - dendrogram_list[i]) > 1:
+					self.num_clust = len(self) - i
+					break
 
 """
 tc = treelet_clust(dat, ker, slice, num_clust)
